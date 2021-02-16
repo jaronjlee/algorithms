2770794c3bb2d06efb8dc20d146af06efb41170a
jaronlee <jaronjlee@gmail.com>
2021-02-15 16:38:33 -0800

    add problems

--- poshmark/3sum.py
+++ poshmark/3sum.py
@@ -27,3 +27,34 @@ class Solution:
                     k -= 1
 
         return result
+
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+#         seen = set()
+#         nums.sort()
+
+#         for i in range(0, len(nums)-2):
+#             if i > 0 and nums[i] == nums[i-1]:
+#                 continue
+#             j = i + 1
+#             k = len(nums) - 1
+#             while j < k:
+#                 num1 = nums[i]
+#                 num2 = nums[j]
+#                 num3 = nums[k]
+#                 currSum = num1 + num2 + num3
+#                 if currSum == 0:
+#                     result.append([num1, num2, num3])
+#                     while j < k and nums[j] == nums[j+1]:
+#                         j += 1
+#                     while k > j and nums[k] == nums[k-1]:
+#                         k -= 1
+#                     j += 1
+#                 elif currSum < 0:
+#                     j += 1
+#                 else:
+#                     k -= 1
+
+#         return result
